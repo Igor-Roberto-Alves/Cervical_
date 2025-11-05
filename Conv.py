2ebe051b18f844c5e200dd89cfd86ce78a258a9d
Igor-Roberto-Alves <al.igor.alves@impatech.edu.br>
2025-11-05 11:16:34 -0300

    last change for poster submission

--- Conv.py
+++ Conv.py
@@ -1,2 +1,11 @@
-def a():
-    pass
+import pandas as pd
+
+# Caminho do arquivo .pkl
+caminho = "dataset_masks_modified.pkl"
+
+# Carregar o dataset
+df = pd.read_pickle(caminho)
+
+# Mostrar as primeiras linhas e todas as colunas
+pd.set_option('display.max_columns', None)
+print(df.head())
